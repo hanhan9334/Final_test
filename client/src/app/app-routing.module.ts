3065bfc210e1147ccf2ebe62716d9fe0c29d628a
John Shin <john.shin53@gmail.com>
2020-11-11 22:39:25 -0500

    took out the duedate

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,23 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { SurveyListComponent } from './survey-list/survey-list.component';
 import { EditComponent } from './pages/edit/edit.component';
 import { AddComponent } from './pages/add/add.component';
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
 /*mport { StoreFirstGuard } from './guards/storeFirst.guard';*/
 
 const routes: Routes = [
 
   {path: 'survey-list', component: SurveyListComponent, data: {title: 'Survey List'}},
   {path: '', redirectTo: '/survey-list', pathMatch: 'full', data: {title: 'Survey List'}},
-<<<<<<< Updated upstream
-  {path: 'edit', component: EditComponent, pathMatch: 'full', data: {title: 'Add New Survey'}},
-  {path: 'add', component: AddComponent, pathMatch: 'full', data: {title: 'Create New Survey'}},
-=======
   {path: 'edit', component: EditComponent, pathMatch: 'full', data: {title: 'Edit Survey'}},
   {path: 'add', component: AddComponent, pathMatch: 'full', data: {title: 'Add New Survey'}},
->>>>>>> Stashed changes
   {path: '**', redirectTo: '/survey-list'}
 
 
