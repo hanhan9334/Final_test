a64a256875345bcad11111faf5e21b70027e9ba4
hanhan9334 <56433113+hanhan9334@users.noreply.github.com>
2020-11-11 21:18:08 -0500

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -29,7 +29,7 @@ const userSchema = new mongoose.Schema({
     surveys:{
         type:Array,
         surveyID:{
-            type:mongoose.Types.ObjectId
+            type:String
         }
     }
 })
